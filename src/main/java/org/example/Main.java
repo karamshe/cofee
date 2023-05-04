dc8749e6f77c4c88f67006cf57cc98509244122d
karamshe <104456125+karamshe@users.noreply.github.com>
2023-05-04 12:05:17 +0300

    Добавили вывод из файлов

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,66 +7,54 @@ public class Main {
     static int coffeePrice[];
 
     public static void main(String[] args) throws IOException {
-        String coffeeMilk = "src/main/resources/Coffee.txt";
-        int coffeeSize = -1;
-        Scanner console = new Scanner(System.in);
-        System.out.println("Размер кофе: 0=маленький 1=средний 2=большой");
+        String coffeeBasket = "src/main/resources/Basket.txt";
+        String Coffee = "src/main/resources/Coffee.txt";
+        String Milk = "src/main/resources/Milk.txt";
+        String Size = "src/main/resources/Size.txt";
+
+        int user = 0;
         while (true) {
+            user+=1;
             System.out.println("Пришёл новый клиент!");
-            System.out.println("Сделайте свой выбор: ");
-            coffeeSize = console.nextInt();
-            if (coffeeSize == 3) {
+
+            System.out.println("Какой размер кофе вы хотите?");
+            System.out.println("Доступные варианты");
+            try (BufferedReader br = new BufferedReader(new FileReader(Size))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                System.err.format("IOException: %s%n", e);
+            }
+
+            System.out.println("Какой вид кофе вы хотите?");
+            System.out.println("Доступные варианты:");
+            try (BufferedReader br = new BufferedReader(new FileReader(Coffee))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                System.err.format("IOException: %s%n", e);
+            }
+
+            System.out.println("Какое молоко использовать?");
+            System.out.println("Доступные варианты:");
+            try (BufferedReader br = new BufferedReader(new FileReader(Milk))) {
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+            } catch (IOException e) {
+                System.err.format("IOException: %s%n", e);
+            }
+            if (user == 1) {
                 break;
             }
-            System.out.println("Добавить капучино на миндальном молоке?");
-            Scanner milk = new Scanner(System.in);
-            String answer = milk.nextLine();
-            if (answer.equalsIgnoreCase("Да")) {
-                FileWriter writer = new FileWriter(coffeeMilk, true);
-                BufferedWriter bufferWriter = new BufferedWriter(writer);
-                bufferWriter.newLine();
-                bufferWriter.write(sizeSelection(coffeeSize) + " руб. Капучино на миндальном молоке.");
-                bufferWriter.close();
-                System.out.println("Добавлено в корзину капучино на миндальном молоке. Итого: " + sizeSelection(coffeeSize) + "руб");
-            } else if (answer.equalsIgnoreCase("Нет")) {
-                System.out.println("Добавить капучино на кокосовом молоке?");
-                answer = milk.nextLine();
-                if (answer.equalsIgnoreCase("Да")) {
-                    FileWriter writer = new FileWriter(coffeeMilk, true);
-                    BufferedWriter bufferWriter = new BufferedWriter(writer);
-                    bufferWriter.newLine();
-                    bufferWriter.write(sizeSelection(coffeeSize) + " руб. Капучино на кокосовом молоке");
-                    bufferWriter.close();
-                    System.out.println("Добавлено в корзину капучино на кокосовом молоке. Итого: " + sizeSelection(coffeeSize) + "руб");
-                }
 
-            } else if (answer.equalsIgnoreCase("Нет")) {
-                System.out.println("Добавить капучино на соевом молоке?");
-                answer = milk.nextLine();
-                if (answer.equalsIgnoreCase("Да")) {
-                    FileWriter writer = new FileWriter(coffeeMilk, true);
-                    BufferedWriter bufferWriter = new BufferedWriter(writer);
-                    bufferWriter.newLine();
-                    bufferWriter.write(sizeSelection(coffeeSize) + " руб. Капучино на соевом молоке");
-                    bufferWriter.close();
-                    System.out.println("Добавлено в корзину капучино на соевом молоке. Итого: " + sizeSelection(coffeeSize) + "руб");
-                }
-            } else {
-                System.out.println("Ошибка");
-
-            }
         }
     }
 
-
-    private static int sizeSelection(int coffeeSize) {
-        if (coffeePrice == null) {
-            coffeePrice = new int[4];
-            coffeePrice[0] = 25;
-            coffeePrice[1] = 50;
-            coffeePrice[2] = 75;
-        }
-        return (coffeePrice[coffeeSize]);
-    }
 }
 
